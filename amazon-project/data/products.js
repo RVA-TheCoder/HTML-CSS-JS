90fb9abfde2ee026c489b9520a20a818bcb256e0
RVA-TheCoder <aakash.sharma00004@gmail.com>
2025-12-08 16:59:07 +0530

    Use inheritance and polymorphism to add size chart

--- amazon-project/data/products.js
+++ amazon-project/data/products.js
@@ -49,7 +49,53 @@ class Product {
     return `$${ formatCurrency(this.priceCents) }`
   }
 
+  extraInfoHTML() {
+    return '' ; 
+  }
+
 }
+
+class Clothing extends Product {
+
+  sizeChartLink ; // undefined
+
+  constructor(productDetails) {
+
+    super(productDetails) ;  // calling or initializing the parent class constructor
+    this.sizeChartLink = this.sizeChartLink ;
+
+  }
+
+  extraInfoHTML() {
+    return `
+      <a href="${this.sizeChartLink}" target="_blank">Size chart</a>
+    ` ;
+  }
+
+
+}
+
+const tshirt = new Clothing({
+    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    rating: {
+      stars: 4.5,
+      count: 56
+    },
+    priceCents: 799,
+    keywords: [
+      "tshirts",
+      "apparel",
+      "mens"
+    ],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png"
+  }) ;
+
+console.log(tshirt) ; 
+console.log(tshirt.getPrice()) ;
+
 
 
 
@@ -714,6 +760,11 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+
+  if (productDetails.type === 'clothing') {
+
+    return new Clothing(productDetails) ; 
+  }
 
   return new Product(productDetails) ;  // returning the instances of class Product
 });
